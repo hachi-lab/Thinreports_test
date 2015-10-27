8b269c294a9d898fe23008697d0ea92b9ca6d532
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-10-27 05:15:15 +0000

    Rubyによる画像の挿入

--- Test.rb
+++ Test.rb
@@ -1,30 +1,21 @@
 # coding: utf-8
 
 require 'thinreports'
+require 'open-uri'
 
 report = Thinreports::Report.new layout: 'Test'
 
 # 1st page
 report.start_new_page
-
-report.page.item(:test).value('Thinreports')
-report.page.item(:test2).value('Thinreports')
+  report.page.item(:test).value('Thinreports')
+  report.page.item(:test2).value('Thinreports')
+  report.page.item(:image).src('Tanaka_Sensei.png')
 
 # 2nd page
 report.start_new_page do |page|
   page.item(:test).value('Ruby').style(:color, '#ff0000')
   page.item(:test2).value('Thinreports')
-end
-
-# 3rd page
-report.start_new_page do
-  item(:test).value('test')
-end
-
-# 4th page
-report.start_new_page do
-  values(test: 'Test',
-         test2: 'Thinreports')
+  page.item(:image).value(open('http://www.prometric-jp.com/career/img/pho_20101020_01.jpg'))
 end
 
 report.generate(filename: 'Test.pdf')
